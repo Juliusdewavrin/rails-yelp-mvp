5eaca1ebc89646df77ad41272bbf77c544513f58
Julius Dewavrin <jdewavrin@gmail.com>
2016-06-01 18:05:00 +0200

    bieautiful style

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
-
+  validates :restaurant, presence: true
   validates :content, presence: true
   validates :rating, presence: true
 end
